87ebbe8a73db284b2057a59c8b954c839074dd5b
songm <songm@rose-hulman.edu>
2015-04-23 14:27:09 -0400

    Cleanred, finsihed tasks that had errors

--- CommandClientVisualStudioTest/AdvancedMockTests.cs
+++ CommandClientVisualStudioTest/AdvancedMockTests.cs
@@ -71,17 +71,27 @@ namespace CommandClientVisualStudioTest
         {
             IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
             Command command = new Command(CommandType.UserExit, ipaddress, null);
-            MemoryStream fakeStream = mocks.DynamicMock<System.IO.MemoryStream>();
+            MemoryStream fakeStream = new MemoryStream();
 
 
             CMDClient client = new CMDClient(null, "Bogus network name");
 
             // we need to set the private variable here
             typeof(CMDClient).GetField("networkStream", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(client, fakeStream);
-
-
-            Assert.AreEqual(true, client.SendCommandToServerUnthreaded(command));
             
+
+            byte[] commandBytes = { 0, 0, 0, 0 };
+            byte[] ipLength = { 9, 0, 0, 0 };
+            byte[] ip = { 49, 50, 55, 46, 48, 46, 48, 46, 49 };
+            byte[] metaDataLength = { 2, 0, 0, 0 };
+            byte[] metaData = { 10, 0 };
+            byte[] test = { 0, 0, 0, 0, 9, 0, 0, 0, 49, 50, 55, 46, 48, 46, 48, 46, 49, 2, 0, 0, 0, 10, 0};
+            
+            
+            Assert.AreEqual(true, client.SendCommandToServerUnthreaded(command));
+
+            CollectionAssert.AreEqual(fakeStream.ToArray(), test);
+
         }
 
         [TestMethod]
@@ -89,14 +99,28 @@ namespace CommandClientVisualStudioTest
         {
             IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
             Command command = new Command(CommandType.UserExit, ipaddress, null);
-            MemoryStream fakeStream = mocks.DynamicMock<System.IO.MemoryStream>();
+            Stream fakeStream = mocks.DynamicMock<System.IO.Stream>();
             System.Threading.Semaphore fakeSemaphore = mocks.DynamicMock<System.Threading.Semaphore>();
 
-            
+            byte[] commandBytes = { 0, 0, 0, 0 };
+            byte[] ipLength = { 9, 0, 0, 0 };
+            byte[] ip = { 49, 50, 55, 46, 48, 46, 48, 46, 49 };
+            byte[] metaDataLength = { 2, 0, 0, 0 };
+            byte[] metaData = { 10, 0 };
+
             using (mocks.Ordered())
             {
                 Expect.Call(fakeSemaphore.WaitOne()).Return(true);
-                
+                fakeStream.Write(commandBytes, 0, 4);
+                fakeStream.Flush();
+                fakeStream.Write(ipLength, 0, 4);
+                fakeStream.Flush();
+                fakeStream.Write(ip, 0, 9);
+                fakeStream.Flush();
+                fakeStream.Write(metaDataLength, 0, 4);
+                fakeStream.Flush();
+                fakeStream.Write(metaData, 0, 2);
+                fakeStream.Flush();
                 Expect.Call(fakeSemaphore.Release()).Return(1);
             }
 
@@ -109,12 +133,12 @@ namespace CommandClientVisualStudioTest
 
 
             Assert.AreEqual(true, client.SendCommandToServerUnthreaded(command));
+
             mocks.VerifyAll();
 
         }
 
         [TestMethod]
-        [ExpectedException(typeof(Exception))]
         public void TestSemaphoreReleaseOnExceptionalOperation()
         {
 
@@ -141,7 +165,7 @@ namespace CommandClientVisualStudioTest
                 fakeStream.Flush();
                 fakeStream.Write(metaData, 0, 2);
                 fakeStream.Flush();
-                LastCall.On(fakeStream).Throw(new Exception());
+                LastCall.On(fakeStream).Throw(new IOException());
                 Expect.Call(fakeSemaphore.Release()).Return(1);
 
             }
@@ -153,8 +177,15 @@ namespace CommandClientVisualStudioTest
             typeof(CMDClient).GetField("networkStream", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(client, fakeStream);
             typeof(CMDClient).GetField("semaphore", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).SetValue(client, fakeSemaphore);
 
-            client.SendCommandToServerUnthreaded(command);
-            
+            try
+            {
+                client.SendCommandToServerUnthreaded(command);
+                Assert.Fail("Did not fail");
+            }
+            catch (IOException)
+            {
+                
+            }
             mocks.VerifyAll();
         }
     }
